187d20ac83a5e5917236c7b09733b97c656ad899
Pauloux <54153283+Pauloux@users.noreply.github.com>
2022-04-03 11:55:48 +0200

    Update main.py

--- Code/Scripts Python/main.py
+++ Code/Scripts Python/main.py
@@ -1,51 +1,10 @@
-import time
+from fonctions_communes_et_parametres import *
 import routine_convertir_donnees_serveur as routine
 import Simulateur_capteur as simu
 import creation_image as image
 import alertes
 
-nom_fichier_logs = "../Documents/logs.txt"
-nom_fichier_erreurs = "../Documents/erreurs.txt"
-
-def get_heure():
-    return time.strftime("%H:%M:%S", time.localtime())
-
-def get_temps():
-    return time.time()
-
-def ecrire_logs(ligne):
-    fichier = open(nom_fichier_logs, "a")
-    ligne = get_heure() + " " + ligne + "\n"
-    fichier.write(ligne)
-    fichier.close()
-
-def ecrire_erreur(ligne):
-    fichier = open(nom_fichier_erreurs, "a")
-    ligne = get_heure() + " " + ligne + "\n"
-    fichier.write(ligne)
-    fichier.close()
-
-def fichier_existe(nom_fichier):
-    try:
-        fichier = open(nom_fichier, "r")
-        fichier.close()
-        return True
-    except IOError as message_erreur:
-        return False
-
-#Création et remise a zéro des fichiers de logs et d'erreurs:
-#Création des fichiers s'ils n'existent pas
-if not fichier_existe(nom_fichier_logs) :
-    fichier_logs = open(nom_fichier_logs, "x")
-    fichier_logs.close()
-if not fichier_existe(nom_fichier_erreurs) :
-    fichier_erreurs = open(nom_fichier_erreurs, "x")
-    fichier_erreurs.close()
-#Remise a zéro des fichiers de logs et d'erreurs
-fichier_logs = open(nom_fichier_logs, "w")
-fichier_logs.close()
-fichier_erreurs = open(nom_fichier_erreurs, "w")
-fichier_erreurs.close()
+verifier_fichiers()
 
 temps_seuil_routine = get_temps()
 temps_seuil_simu = get_temps()
