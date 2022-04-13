b5b65955406572fee01268b97d63a83ccd56c2ab
Pauloux <54153283+Pauloux@users.noreply.github.com>
2022-04-13 11:04:13 +0200

    Update main.py

--- Code/Scripts Python/main.py
+++ Code/Scripts Python/main.py
@@ -1,27 +1,33 @@
+#Importation des autres fichiers
 from fonctions_communes_et_parametres import *
 import routine_convertir_donnees_serveur as routine
 import Simulateur_capteur as simu
 import creation_image as image
 import alertes
 
+#On vérifie que les fichiers existent
 verifier_fichiers()
 
+#Initialisation des timers
 temps_seuil_routine = get_temps()
 temps_seuil_simu = get_temps()
 temps_seuil_image = get_temps()
 temps_seuil_alerte = get_temps()
-#En secondes
+
+#Définition des délais d'exécution(en secondes)
 acceleration = 20
 delai_routine = 120 / acceleration
 delai_simu = 10 / acceleration
 delai_image = 120 / acceleration
 delai_alerte = 120 / acceleration
 
+#Uniquement pour le simulateur
 #Récupère le nombre de lignes du fichier de sortie pour savoir quelle ligne récuperer dans le fichier d'entrée ensuite
 numero_ligne = 0
 simu.initialisation()
 
 while True:
+    #Déclenchement des autres fichiers au bon moment
     temps_actu = get_temps()
     if temps_actu >= temps_seuil_routine:
         routine.lancer()
@@ -35,5 +41,5 @@ while True:
     if temps_actu >= temps_seuil_alerte:
         alertes.lancer()
         temps_seuil_alerte += delai_alerte
-    #Pour ne pas sur-utiliser le processeur
+    #On attend un peu pour ne pas sur-utiliser le processeur
     time.sleep(1 / acceleration)
