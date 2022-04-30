a1bdaadd108edca137db65923866149ad7e16ccc
Pauloux <54153283+Pauloux@users.noreply.github.com>
2022-04-30 19:27:14 +0200

    Update main.py

--- Code/Scripts Python/main.py
+++ Code/Scripts Python/main.py
@@ -1,11 +1,10 @@
-#Importation des autres fichiers
 from fonctions_communes_et_parametres import *
 import routine_convertir_donnees_serveur as routine
 import Simulateur_capteur as simu
 import creation_image as image
 import alertes
 
-#On vérifie que les fichiers existent
+#Vérification que les fichiers existes
 print("Vérification des fichiers...", end=" ")
 verifier_fichiers_et_initialisation()
 print("Fichiers vérifiés !")
@@ -17,7 +16,7 @@ temps_seuil_image = get_temps()
 temps_seuil_alerte = get_temps()
 
 #Définition des délais d'exécution(en secondes)
-acceleration = 10
+acceleration = 1 #A retirer lors de la fin du projet, uniquement pour les tests
 delai_routine = 120 / acceleration
 delai_simu = 10 / acceleration
 delai_image = 120 / acceleration
@@ -31,14 +30,16 @@ simu.initialisation()
 print("Simulateur initialisé !")
 
 print("Lancement du programme !")
+
 while True:
-    #Vérifie si c'est la bonne heure
+    #Vérifie si c'est la bonne heure pour déclencher le programme
     if not heure_pour_declencher():
         remplacer_images_capteur_arrete()
-        #Attends 1 minute avant de revérifier car c'est un état de veille
+        #Attends 1 minute avant de revérifier
         time.sleep(60)
         continue
-    #Déclenchement des autres fichiers au bon moment
+
+    #Déclenchement des autres scripts au bon moment
     temps_actu = get_temps()
     if temps_actu >= temps_seuil_routine:
         routine.lancer()
@@ -52,5 +53,6 @@ while True:
     if temps_actu >= temps_seuil_alerte:
         alertes.lancer()
         temps_seuil_alerte += delai_alerte
+        
     #On attend un peu pour ne pas sur-utiliser le processeur
     time.sleep(1 / acceleration)
